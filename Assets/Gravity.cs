429b1533abb597c13a64896ea2b10518110a19c9
Robinerd <robinerd@gmail.com>
2017-04-23 16:59:23 +0200

    Can run around in world and jump.

---
+++ Assets/Gravity.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+    public float gravity = 10;
+
+    float radius;
+
+    // Use this for initialization
+    void Start () {
+        radius = transform.localScale.x * GetComponent<SphereCollider>().radius;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		foreach(Rigidbody body in FindObjectsOfType<Rigidbody>())
+        {
+            if (body.isKinematic)
+                continue;
+
+            Vector3 fromCenter = body.position - transform.position;
+            Vector3 up = -(fromCenter.normalized);
+
+            //HANDLE GRAVITY
+            body.AddForce(-up * gravity, ForceMode.Acceleration);
+
+            Vector3 axis = Vector3.Cross(body.transform.up, up) * 400;
+            Vector3 torque = axis * Vector3.Angle(body.transform.up, up); 
+            body.AddTorque(axis, ForceMode.Acceleration);
+            //body.transform.Rotate(Quaternion.FromToRotation(body.transform.up, up).eulerAngles, Space.World);
+        }
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Player : MonoBehaviour {
+
+    public MouseLook mouseLook;
+    public float ForwardSpeed = 8.0f;   // Speed when walking forward
+    public float BackwardSpeed = 4.0f;  // Speed when walking backwards
+    public float StrafeSpeed = 4.0f;    // Speed when walking sideways
+    public float JumpForce = 30f;
+
+    // Use this for initialization
+    void Start()
+    {
+        mouseLook.Init(transform, Camera.main.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseLook.LookRotation(transform, Camera.main.transform);
+        
+    }
+
+    void FixedUpdate()
+    {
+        mouseLook.UpdateCursorLock();
+    }
+}
